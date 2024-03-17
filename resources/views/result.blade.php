5cf7b666f6116568dd8ad3df4e24028d7d9b0a2b
knk-summer <163017021+knk-summer@users.noreply.github.com>
2024-03-17 21:57:43 +0900

    完成品表示画面の作成

---
+++ resources/views/result.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>じぶんステッカー</title>
+<link rel="stylesheet" href="{{  asset('css/reset.css') }}" />
+<link rel="stylesheet" href="{{  asset('css/style.css') }}" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="wrap">
+  <div class="container">
+    <div class="contents">
+      <h1 class="logo"><img src="{{ asset('img/logo.gif') }}" alt="じぶんステッカー"></h1>
+      <div class="mv"><img src="{{ asset('img/main.png') }}" alt="仮"></div>
+      <h2 class="headcopy">ここにはヘッドコピーが入ります。<br>
+        ここにはヘッドコピーが入ります。<br>
+        ここにはヘッドコピーが入ります。<br>
+        ここにはヘッドコピーが入ります。<br>
+        ここにはヘッドコピーが入ります。<br>
+        ここにはヘッドコピーが入ります。</h2>
+      <button class="start_btn">はじめる</button>
+    </div>
+  </div>
+</div>
+</body>
+</html>
